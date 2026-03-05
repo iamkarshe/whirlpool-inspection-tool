48607f693cc4102690fd496df351055d4eb17463
Utkarsh Kumar Raut <utkarsh2point0@gmail.com>
2026-03-05 18:28:42 +0530

    [update] Refactor SiteHeader and AppSidebar components for improved styling and organization; update navigation items and remove unused imports for cleaner code structure.

--- ui/src/components/layout/sidebar/app-sidebar.tsx
+++ ui/src/components/layout/sidebar/app-sidebar.tsx
@@ -1,28 +1,7 @@
-"use client";
-
-import { useIsTablet } from "@/hooks/use-mobile";
-import { PlusIcon } from "@radix-ui/react-icons";
-import { ChevronsUpDown, ShoppingBagIcon, UserCircle2Icon } from "lucide-react";
-import * as React from "react";
-import { useEffect } from "react";
-
-import Logo from "@/components/layout/logo";
 import { NavMain } from "@/components/layout/sidebar/nav-main";
 import { NavUser } from "@/components/layout/sidebar/nav-user";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import {
   DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -36,7 +15,8 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { Link } from "react-router-dom";
+import { useIsTablet } from "@/hooks/use-mobile";
+import { useEffect } from "react";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { setOpen, setOpenMobile, isMobile } = useSidebar();
@@ -58,47 +38,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton className="hover:text-foreground h-10 group-data-[collapsible=icon]:px-0!">
-                  <Logo />
-                  <span className="text-foreground font-semibold">
-                    Shadcn UI Kit
+                  <span className="text-foreground font-semibold text-xl">
+                    Whirlpool Insights
                   </span>
-                  <ChevronsUpDown className="ml-auto group-data-[collapsible=icon]:hidden" />
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="mt-4 w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align="end"
-                sideOffset={4}
-              >
-                <DropdownMenuLabel>Projects</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="flex items-center gap-3">
-                  <div className="flex size-8 items-center justify-center rounded-md border">
-                    <ShoppingBagIcon className="text-muted-foreground size-4" />
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="text-sm font-medium">E-commerce</span>
-                    <span className="text-xs text-green-700">Active</span>
-                  </div>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="flex items-center gap-3">
-                  <div className="flex size-8 items-center justify-center rounded-md border">
-                    <UserCircle2Icon className="text-muted-foreground size-4" />
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="text-sm font-medium">Blog Platform</span>
-                    <span className="text-muted-foreground text-xs">
-                      Inactive
-                    </span>
-                  </div>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <Button size="sm" className="w-full">
-                  <PlusIcon />
-                  New Project
-                </Button>
-              </DropdownMenuContent>
             </DropdownMenu>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -109,23 +53,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </ScrollArea>
       </SidebarContent>
       <SidebarFooter>
-        <Card className="gap-4 overflow-hidden py-4 group-data-[collapsible=icon]:hidden">
-          <CardHeader className="px-3">
-            <CardTitle>Unlock Everything</CardTitle>
-            <CardDescription>
-              Get instant access to all premium dashboards, templates, and UI
-              components. Pay once, use forever in unlimited projects.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="px-3">
-            <Button className="w-full" asChild>
-              <Link to="https://shadcnuikit.com/pricing" target="_blank">
-                <span className="size-2 shrink-0 rounded-full bg-green-500"></span>
-                Get Full Access
-              </Link>
-            </Button>
-          </CardContent>
-        </Card>
         <NavUser />
       </SidebarFooter>
     </Sidebar>
--- ui/src/components/ui/custom/tiptap/extensions/image.tsx
+++ ui/src/components/ui/custom/tiptap/extensions/image.tsx
@@ -2,7 +2,11 @@
 // @ts-nocheck
 /* eslint-disable */
 import Image from "@tiptap/extension-image";
-import { type NodeViewProps, NodeViewWrapper, ReactNodeViewRenderer } from "@tiptap/react";
+import {
+  type NodeViewProps,
+  NodeViewWrapper,
+  ReactNodeViewRenderer,
+} from "@tiptap/react";
 import {
   AlignCenter,
   AlignLeft,
@@ -12,7 +16,7 @@ import {
   Trash,
   Edit,
   ImageIcon,
-  Loader2
+  Loader2,
 } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
@@ -25,7 +29,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuSub,
   DropdownMenuSubContent,
-  DropdownMenuSubTrigger
+  DropdownMenuSubTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
@@ -36,35 +40,35 @@ export const ImageExtension = Image.extend({
   addAttributes() {
     return {
       src: {
-        default: null
+        default: null,
       },
       alt: {
-        default: null
+        default: null,
       },
       title: {
-        default: null
+        default: null,
       },
       width: {
-        default: "100%"
+        default: "100%",
       },
       height: {
-        default: null
+        default: null,
       },
       align: {
-        default: "center"
+        default: "center",
       },
       caption: {
-        default: ""
+        default: "",
       },
       aspectRatio: {
-        default: null
-      }
+        default: null,
+      },
     };
   },
 
   addNodeView: () => {
     return ReactNodeViewRenderer(TiptapImage);
-  }
+  },
 });
 
 function TiptapImage(props: NodeViewProps) {
@@ -72,7 +76,9 @@ function TiptapImage(props: NodeViewProps) {
   const imageRef = useRef<HTMLImageElement | null>(null);
   const nodeRef = useRef<HTMLDivElement | null>(null);
   const [resizing, setResizing] = useState(false);
-  const [resizingPosition, setResizingPosition] = useState<"left" | "right">("left");
+  const [resizingPosition, setResizingPosition] = useState<"left" | "right">(
+    "left",
+  );
   const [resizeInitialWidth, setResizeInitialWidth] = useState(0);
   const [resizeInitialMouseX, setResizeInitialMouseX] = useState(0);
   const [editingCaption, setEditingCaption] = useState(false);
@@ -81,21 +87,27 @@ function TiptapImage(props: NodeViewProps) {
   const [imageUrl, setImageUrl] = useState("");
   const [altText, setAltText] = useState(node.attrs.alt || "");
 
-  const { previewUrl, fileInputRef, handleFileChange, handleRemove, uploading, error } =
-    useImageUpload({
-      onUpload: (imageUrl) => {
-        updateAttributes({
-          src: imageUrl,
-          alt: altText || fileInputRef.current?.files?.[0]?.name
-        });
-        handleRemove();
-        setOpenedMore(false);
-      }
-    });
+  const {
+    previewUrl,
+    fileInputRef,
+    handleFileChange,
+    handleRemove,
+    uploading,
+    error,
+  } = useImageUpload({
+    onUpload: (imageUrl) => {
+      updateAttributes({
+        src: imageUrl,
+        alt: altText || fileInputRef.current?.files?.[0]?.name,
+      });
+      handleRemove();
+      setOpenedMore(false);
+    },
+  });
 
   function handleResizingPosition({
     e,
-    position
+    position,
   }: {
     e: React.MouseEvent<HTMLDivElement, MouseEvent>;
     position: "left" | "right";
@@ -126,7 +138,7 @@ function TiptapImage(props: NodeViewProps) {
 
     if (newWidth < parentWidth) {
       updateAttributes({
-        width: newWidth
+        width: newWidth,
       });
     }
   }
@@ -137,7 +149,10 @@ function TiptapImage(props: NodeViewProps) {
     setResizeInitialWidth(0);
   }
 
-  function handleTouchStart(event: React.TouchEvent, position: "left" | "right") {
+  function handleTouchStart(
+    event: React.TouchEvent,
+    position: "left" | "right",
+  ) {
     event.preventDefault();
     setResizing(true);
     setResizingPosition(position);
@@ -150,9 +165,12 @@ function TiptapImage(props: NodeViewProps) {
   function handleTouchMove(event: TouchEvent) {
     if (!resizing) return;
 
-    let dx = (event.touches[0]?.clientX ?? resizeInitialMouseX) - resizeInitialMouseX;
+    let dx =
+      (event.touches[0]?.clientX ?? resizeInitialMouseX) - resizeInitialMouseX;
     if (resizingPosition === "left") {
-      dx = resizeInitialMouseX - (event.touches[0]?.clientX ?? resizeInitialMouseX);
+      dx =
+        resizeInitialMouseX -
+        (event.touches[0]?.clientX ?? resizeInitialMouseX);
     }
 
     const newWidth = Math.max(resizeInitialWidth + dx, 150);
@@ -160,7 +178,7 @@ function TiptapImage(props: NodeViewProps) {
 
     if (newWidth < parentWidth) {
       updateAttributes({
-        width: newWidth
+        width: newWidth,
       });
     }
   }
@@ -191,7 +209,7 @@ function TiptapImage(props: NodeViewProps) {
     if (imageUrl) {
       updateAttributes({
         src: imageUrl,
-        alt: altText
+        alt: altText,
       });
       setImageUrl("");
       setAltText("");
@@ -220,10 +238,16 @@ function TiptapImage(props: NodeViewProps) {
         selected ? "border-blue-300" : "",
         node.attrs.align === "left" && "left-0 translate-x-0",
         node.attrs.align === "center" && "left-1/2 -translate-x-1/2",
-        node.attrs.align === "right" && "left-full -translate-x-full"
+        node.attrs.align === "right" && "left-full -translate-x-full",
       )}
-      style={{ width: node.attrs.width }}>
-      <div className={cn("group relative flex flex-col rounded-md", resizing && "")}>
+      style={{ width: node.attrs.width }}
+    >
+      <div
+        className={cn(
+          "group relative flex flex-col rounded-md",
+          resizing && "",
+        )}
+      >
         <figure className="relative m-0">
           <img
             ref={imageRef}
@@ -245,7 +269,8 @@ function TiptapImage(props: NodeViewProps) {
                 onMouseDown={(event) => {
                   handleResizingPosition({ e: event, position: "left" });
                 }}
-                onTouchStart={(event) => handleTouchStart(event, "left")}>
+                onTouchStart={(event) => handleTouchStart(event, "left")}
+              >
                 <div className="z-20 h-[70px] w-1 rounded-xl border bg-[rgba(0,0,0,0.65)] opacity-0 transition-all group-hover:opacity-100" />
               </div>
               <div
@@ -254,7 +279,8 @@ function TiptapImage(props: NodeViewProps) {
                 onMouseDown={(event) => {
                   handleResizingPosition({ e: event, position: "right" });
                 }}
-                onTouchStart={(event) => handleTouchStart(event, "right")}>
+                onTouchStart={(event) => handleTouchStart(event, "right")}
+              >
                 <div className="z-20 h-[70px] w-1 rounded-xl border bg-[rgba(0,0,0,0.65)] opacity-0 transition-all group-hover:opacity-100" />
               </div>
             </>
@@ -274,7 +300,8 @@ function TiptapImage(props: NodeViewProps) {
         ) : (
           <div
             className="text-muted-foreground mt-2 cursor-text text-center text-sm"
-            onClick={() => editor?.isEditable && setEditingCaption(true)}>
+            onClick={() => editor?.isEditable && setEditingCaption(true)}
+          >
             {caption || "Add a caption..."}
           </div>
         )}
@@ -282,29 +309,42 @@ function TiptapImage(props: NodeViewProps) {
         {editor?.isEditable && (
           <div
             className={cn(
-              "bg-background/80 absolute top-4 right-4 flex items-center gap-1 rounded-md border p-1 opacity-0 backdrop-blur transition-opacity",
+              "bg-background/80 absolute top-4 right-4 flex items-center gap-1 rounded-md border p-1 opacity-0 transition-opacity",
               !resizing && "group-hover:opacity-100",
-              openedMore && "opacity-100"
-            )}>
+              openedMore && "opacity-100",
+            )}
+          >
             <Button
               size="icon"
-              className={cn("size-7", node.attrs.align === "left" && "bg-accent")}
+              className={cn(
+                "size-7",
+                node.attrs.align === "left" && "bg-accent",
+              )}
               variant="ghost"
-              onClick={() => updateAttributes({ align: "left" })}>
+              onClick={() => updateAttributes({ align: "left" })}
+            >
               <AlignLeft className="size-4" />
             </Button>
             <Button
               size="icon"
-              className={cn("size-7", node.attrs.align === "center" && "bg-accent")}
+              className={cn(
+                "size-7",
+                node.attrs.align === "center" && "bg-accent",
+              )}
               variant="ghost"
-              onClick={() => updateAttributes({ align: "center" })}>
+              onClick={() => updateAttributes({ align: "center" })}
+            >
               <AlignCenter className="size-4" />
             </Button>
             <Button
               size="icon"
-              className={cn("size-7", node.attrs.align === "right" && "bg-accent")}
+              className={cn(
+                "size-7",
+                node.attrs.align === "right" && "bg-accent",
+              )}
               variant="ghost"
-              onClick={() => updateAttributes({ align: "right" })}>
+              onClick={() => updateAttributes({ align: "right" })}
+            >
               <AlignRight className="size-4" />
             </Button>
             <Separator orientation="vertical" className="h-[20px]" />
@@ -314,7 +354,11 @@ function TiptapImage(props: NodeViewProps) {
                   <MoreVertical className="size-4" />
                 </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent align="start" alignOffset={-90} className="mt-1 text-sm">
+              <DropdownMenuContent
+                align="start"
+                alignOffset={-90}
+                className="mt-1 text-sm"
+              >
                 <DropdownMenuItem onClick={() => setEditingCaption(true)}>
                   <Edit className="mr-2 size-4" /> Edit Caption
                 </DropdownMenuItem>
@@ -336,7 +380,8 @@ function TiptapImage(props: NodeViewProps) {
                         />
                         <label
                           htmlFor="replace-image-upload"
-                          className="hover:bg-accent flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 border-dashed p-4">
+                          className="hover:bg-accent flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border-2 border-dashed p-4"
+                        >
                           {uploading ? (
                             <>
                               <Loader2 className="h-4 w-4 animate-spin" />
@@ -349,7 +394,11 @@ function TiptapImage(props: NodeViewProps) {
                             </>
                           )}
                         </label>
-                        {error && <p className="text-destructive mt-2 text-xs">{error}</p>}
+                        {error && (
+                          <p className="text-destructive mt-2 text-xs">
+                            {error}
+                          </p>
+                        )}
                       </div>
 
                       <div>
@@ -365,7 +414,8 @@ function TiptapImage(props: NodeViewProps) {
                             onClick={handleImageUrlSubmit}
                             className="w-full"
                             disabled={!imageUrl}
-                            size="sm">
+                            size="sm"
+                          >
                             Replace with URL
                           </Button>
                         </div>
@@ -387,19 +437,22 @@ function TiptapImage(props: NodeViewProps) {
                   onClick={() => {
                     const aspectRatio = node.attrs.aspectRatio;
                     if (aspectRatio) {
-                      const parentWidth = nodeRef.current?.parentElement?.offsetWidth ?? 0;
+                      const parentWidth =
+                        nodeRef.current?.parentElement?.offsetWidth ?? 0;
                       updateAttributes({
                         width: parentWidth,
-                        height: parentWidth / aspectRatio
+                        height: parentWidth / aspectRatio,
                       });
                     }
-                  }}>
+                  }}
+                >
                   <Maximize className="mr-2 size-4" /> Full Width
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   className="text-destructive focus:text-destructive"
-                  onClick={deleteNode}>
+                  onClick={deleteNode}
+                >
                   <Trash className="mr-2 size-4" /> Delete Image
                 </DropdownMenuItem>
               </DropdownMenuContent>
