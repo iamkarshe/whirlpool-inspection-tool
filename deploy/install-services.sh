3cbbc702dfc532190044fdda06baf6e2d8d0d7a8
Utkarsh Kumar Raut <utkarsh2point0@gmail.com>
2026-06-05 09:34:00 +0530

    [deploy] systemd service added.

--- deploy/install-services.sh
+++ deploy/install-services.sh
@@ -1,4 +1,6 @@
-sudo cp deploy/*.service /etc/systemd/system/
+sudo useradd --system --create-home --shell /usr/sbin/nologin whirlpool
+
+sudo cp *.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable --now redis-server
 sudo systemctl enable --now whirlpool-api-fastapi whirlpool-api-celery whirlpool-api-flower
